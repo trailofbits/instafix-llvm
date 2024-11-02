3777dfc7c9d912121da5d93aa39537738e1714ab
xlauko <xlauko@mail.muni.cz>
2024-11-02 20:12:19 +0100

    [mlir][linkage] Introduce linkage dialect

---
+++ mlir/include/mlir/Dialect/Linkage/IR/Linkage.h
@@ -0,0 +1,16 @@
+//===- Linkage.h - Linkage Dialect ------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_LINKAGE_IR_LINKAGE_H
+#define MLIR_DIALECT_LINKAGE_IR_LINKAGE_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "mlir/Dialect/Linkage/IR/Linkage.h.inc"
+
+#endif // MLIR_DIALECT_LINKAGE_IR_LINKAGE_H
