a9f87565ba9c0608b7937fffa449944cbf697c18
2over12 <ian.smith@trailofbits.com>
2025-04-03 13:39:43 -0400

    expose builtin linker

--- mlir/include/mlir/IR/BuiltinLinkerInterface.h
+++ mlir/include/mlir/IR/BuiltinLinkerInterface.h
@@ -13,10 +13,33 @@
 #ifndef MLIR_IR_BUILTINLINKERINTERFACE_H
 #define MLIR_IR_BUILTINLINKERINTERFACE_H
 
+#include "mlir/Linker/LinkerInterface.h"
+
 namespace mlir {
 class DialectRegistry;
 
 namespace builtin {
+using namespace mlir::link;
+class BuiltinLinkerInterface : public ModuleLinkerInterface {
+public:
+  using ModuleLinkerInterface::ModuleLinkerInterface;
+
+    LogicalResult initialize(ModuleOp src) override;
+
+    LogicalResult summarize(ModuleOp src, unsigned flags) override;
+
+    LogicalResult summarize(Operation *op, unsigned flags, bool forDependency);
+
+    LogicalResult link(LinkState &state) const override;
+
+    OwningOpRef<ModuleOp> createCompositeModule(ModuleOp src) override;
+  
+
+private:
+  SymbolLinkerInterfaces symbolLinkers;
+};
+
+
 void registerLinkerInterface(DialectRegistry &registry);
 } // namespace builtin
 
