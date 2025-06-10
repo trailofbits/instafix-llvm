14b6c5c170d2e49a74390e0eca206d20605a92da
Robert Konicar <robert.konicar@trailofbits.com>
2025-06-10 19:25:44 +0200

    [MLIR][mlir-link] Fix weak linkage for two weak definitions

--- mlir/include/mlir/Linker/LLVMLinkerMixin.h
+++ mlir/include/mlir/Linker/LLVMLinkerMixin.h
@@ -290,10 +290,9 @@ public:
     if (isLinkOnceLinkage(srcLinkage))
       return ConflictResolution::LinkFromDst;
 
-    if (isLinkOnceLinkage(dstLinkage) || isWeakLinkage(dstLinkage))
-      return ConflictResolution::LinkFromSrc;
-
     if (isCommonLinkage(srcLinkage)) {
+      if (isLinkOnceLinkage(dstLinkage) || isWeakLinkage(dstLinkage))
+        return ConflictResolution::LinkFromSrc;
       if (!isCommonLinkage(dstLinkage))
         return ConflictResolution::LinkFromDst;
       if (derived.getBitWidth(pair.src) > derived.getBitWidth(pair.dst))
@@ -306,10 +305,9 @@ public:
       assert(!isAvailableExternallyLinkage(dstLinkage));
       if (isLinkOnceLinkage(dstLinkage) && isWeakLinkage(srcLinkage)) {
         return ConflictResolution::LinkFromSrc;
-      } else {
-        // No need to link the `src`
-        return ConflictResolution::LinkFromDst;
       }
+      // No need to link the `src`
+      return ConflictResolution::LinkFromDst;
     }
 
     if (isWeakForLinker(dstLinkage)) {
